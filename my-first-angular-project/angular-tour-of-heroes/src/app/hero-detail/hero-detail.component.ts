dfe4e8e5fc9bf56f8643e4c8c36063ecd7b594c0
Josenildo Coutinho Lins <josenildo@plano-ortoclin.com>
2018-10-16 18:41:07 -0300

    asass

--- my-first-angular-project/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ my-first-angular-project/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -19,13 +19,22 @@ export class HeroDetailComponent implements OnInit {
     private heroService: HeroService,
     private location: Location) { }
 
-    ngOnInit(): void {
-      //this.getHero();
-    }
-    
-    // getHero(): void {
-    //   const id = +this.route.snapshot.paramMap.get('id');
-    //   this.heroService.getHero(id)
-    //     .subscribe(hero => this.hero = hero);
-    // }
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
--- my-first-angular-project/angular-tour-of-heroes/src/app/in-memory-data.service.ts
+++ my-first-angular-project/angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -15,7 +15,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return {heroes};
+    return { heroes };
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
@@ -26,4 +26,6 @@ export class InMemoryDataService implements InMemoryDbService {
   genId(heroes: Hero[]): number {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
+
+  
 }
